4fabd61fcb3d9b783a1e757d3b0714052539a252
Himani Jha <himanijha12@gmail.com>
2025-05-09 18:18:36 -0700

    Added basic authentication

--- fitnessmania/backend/server.js
+++ fitnessmania/backend/server.js
@@ -9,6 +9,19 @@ const app = express();
 
 app.use(cors());
 
+app.use((req, res, next) => {
+    const username = "admin"; 
+    const password = "password"; // hard coded password
+    if (req.headers.authorization == `Basic ${btoa(username + ":" + password)}`) {
+        return next();
+    } else {
+        res.set('WWW-Authenticate', 'Basic realm="Everything"');
+        res.status(401).send('Authentication required.');
+    }
+
+    // TODO: hash password instead of storing as text
+});
+
 app.get('/', (req, res) => {
     res.send('Hello World - the backend');
 });
