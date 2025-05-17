4d9ebac60567b815b48b8fdb1a330455a2ffddb3
AnokhiLuthra <luthra.anokhi@gmail.com>
2025-05-16 20:49:33 -0700

    landing page

--- fitnessmania/src/App.js
+++ fitnessmania/src/App.js
@@ -1,5 +1,7 @@
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import UserForm from './components/UserForm';
+import LandingPage from './LandingPage';
 
 function App() {
   const [data, setData] = useState('');
@@ -15,24 +17,36 @@ function App() {
       .then(response => response.json())
       .then(users => setUsers(users))
       .catch(error => console.error('Error fetching users:', error));
-
   }, []);
 
   return (
-    <div>
-      <h1>This is FitnessMania.</h1>
-      <p>{data}</p>
-      <UserForm />
-      <h2>Users</h2>
-      <ul>
-        {users.map(user => (
-          <li key={user._id}>{user.username} ({user.email})</li>
-        ))}
-      </ul>
-    </div>
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div className="p-4">
+              <Link to="/landing">
+                <button className="mb-4 px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700">
+                  Go to Landing Page
+                </button>
+              </Link>
+              <h1>This is FitnessMania.</h1>
+              <p>{data}</p>
+              <UserForm />
+              <h2>Users</h2>
+              <ul>
+                {users.map(user => (
+                  <li key={user._id}>{user.username} ({user.email})</li>
+                ))}
+              </ul>
+            </div>
+          }
+        />
+        <Route path="/landing" element={<LandingPage />} />
+      </Routes>
+    </Router>
   );
 }
 
 export default App;
-
-
